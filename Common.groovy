b621378641653080b405e4f099227c7e2d690a54
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-01-13 17:59:28 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -61,9 +61,9 @@ def create_name (def vmname){
        echo "Terminated VM Present"
        else
        Ip=$(sudo aws ec2 describe-instances --instance-ids="$p"  --query 'Reservations[*].Instances[*].{Instance:PublicIpAddress}')
-       sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/Jenkins.pem" "$user"@"$Ip"
-       sudo scp -i "/tmp/Jenkins.pem" /tmp/remotescript.sh "$user"@"$Ip":/tmp
-       sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/Jenkins.pem" "$user"@"$Ip" 'bash -s' < /tmp/remotescript.sh
+       sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/Jenkins.pem" "$user"@$Ip
+       sudo scp -i "/tmp/Jenkins.pem" /tmp/remotescript.sh "$user"@$Ip:/tmp
+       sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/Jenkins.pem" "$user"@$Ip 'bash -s' < /tmp/remotescript.sh
        fi
        done < name.txt
      '''
