fd0c225442bb2e474c6306da926597a6aa82b36d
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-01-13 14:50:01 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -21,17 +21,20 @@ def vm_creation (def imageid,def vmcount,def vmtype,def keyname,def securitygrou
     sudo aws ec2 run-instances --image-id ${imageid} --block-device-mappings file:///tmp/maping.json --count ${vmcount} --instance-type ${vmtype} --key-name ${keyname} --security-group-ids ${securitygroupid} --subnet-id ${subnetid}
 """
 }
-def create_name (def instanceid,def vmname){
+def create_name (def vmname){
   sh '''#!/bin/bash -xe
         >name.txt
-        #sudo aws ec2 describe-instances --output json | grep InstanceId | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
+        sudo aws ec2 describe-instances --output json | grep InstanceId | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
       
-        #for a in instanceid=$(cat name.txt); do
+       while IFS="" read -r p || [ -n "$p" ]
+       do
+      printf '%s\n' "$p"
+      done < name.txt
        # echo "$instanceid"
-        #echo "sudo aws ec2 create-tags --resources \$instanceid --tags Key=Name,Value=\$vmname"
-       # done
-       myvar=sachin
-       echo \$myvar
+       # echo "sudo aws ec2 create-tags --resources \$instanceid --tags Key=Name,Value=\$vmname"
+        
+       #myvar=sachin
+       #echo \$myvar
     '''
  }
  
