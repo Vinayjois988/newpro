46162c4b1df17dbe1efa0c7ef77af05750d3b333
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-01-13 14:33:05 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -23,7 +23,7 @@ def vm_creation (def imageid,def vmcount,def vmtype,def keyname,def securitygrou
 }
 def create_name (def instanceid,def vmname){
   sh '''
-    
+     #!/bin/bash
      >name.txt
      sudo aws ec2 describe-instances --output json | grep InstanceId | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
       
