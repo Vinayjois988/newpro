42e8256d3270999480a7978d345097e34847c3ea
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-01-19 09:40:26 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -115,7 +115,9 @@ def cluster(){
 }
 def get_cluster(def jenkinsid, def okd, def user){
  sh " set +x "
- sh "echo ${jenkinsid}"
+ sh '''
+echo ${jenkinsid}
+  '''
       
      sh '''
      sudo aws ec2 describe-instances --output json | grep InstanceId | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
